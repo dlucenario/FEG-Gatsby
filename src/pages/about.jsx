0592b474d7739ec0e7a0ba0d9737a8b7739e5ae0
Dom <d_lucenario@yahoo.com.ph>
2021-11-10 22:41:51 +0800

    test yotpo

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,20 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 
+import Yotpo from '../components/atoms/SampleYotpo';
+
 const AboutPage = () => (
   // <Layout pageTitle="About Me">
     
   // </Layout>
   <div>
     Sample Yotpo Integration
-    <div class="yotpo yotpo-main-widget"
-      data-product-id="1957"
-      data-price="50"
-      data-currency="USD"
-      data-name="Soap"
-      data-url="www.item.com/soap"
-      data-image-url="www.url.com">
-    </div>
+    <Yotpo></Yotpo>
   </div>
 );
 
